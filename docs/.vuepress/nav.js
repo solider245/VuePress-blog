6c951532b083e1fbaf03e7e5d1d255e11b6e773c
root <root@solider245.localdomain>
2020-07-29 11:01:40 +0800

    重新生成了nav.js

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,4 +1,10 @@
 module.exports = [
-  { text: "about", items: [{ text: "about", link: "/about/" }] },
-  { text: "blog", items: [{ text: "blog", link: "/blog/" }] },
+    {
+        text: "about", items: [
+            { text: "about", link: "/about/" }]
+    },
+    {
+        text: "blog", items: [
+            { text: "blog", link: "/blog/" }]
+    },
 ];
