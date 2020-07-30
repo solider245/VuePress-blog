69d0f56bbd42ef87877a49b208f199a01f1cdffe
root <root@solider245.localdomain>
2020-07-30 16:35:17 +0800

    修改了markdown和plugins模块

--- docs/.vuepress/config/PluginsConfig.js
+++ docs/.vuepress/config/PluginsConfig.js
@@ -1,3 +1,18 @@
-module.exports ={
-    
-}
+const autonav_options = {
+  enable: true,
+};
+
+module.exports = {
+  plugins: [
+    //[ 'autonav', autonav_options ]
+    [
+      "@vuepress/back-to-top", //back-to-top 插件
+      "@vuepress/blog",
+      "@vuepress/active-header-links", // 页面滚动时自动激活侧边栏链接的插件
+      {
+        sidebarLinkSelector: ".sidebar-link",
+        headerAnchorSelector: ".header-anchor",
+      },
+    ],
+  ],
+};
---
+++ docs/.vuepress/config/markdownConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    markdown: {
+      // markdown-it-anchor 的选项
+      anchor: { permalink: false },
+      //代码块显示行号
+      lineNumbers: true ,
+      // markdown-it-toc 的选项
+      toc: { includeLevel: [1, 2] }
+      //extendMarkdown: md => {
+        // 使用更多的 markdown-it 插件!
+        //md.use(require('markdown-it-xxx'))
+     // }
+    }
+  }
