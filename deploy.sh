a43761304b8df9ff6e2911de3857fe611ecd9a4a
root <root@solider245.localdomain>
2020-07-31 03:51:20 +0800

    修改了脚本文件，可以推送到原来的文件地址了。

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # 确保脚本抛出遇到的错误
 set -e
 git pull origin master
-git push -f github master
+#git push -f github master
 # 生成静态文件
 npm run docs:build
 
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 git push -f git@github.com:solider245/solider245.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:solider245/VuePress-blog.git master:gh-pages
 
 cd -
