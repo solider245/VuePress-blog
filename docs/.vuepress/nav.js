cc002bdcb5cf6594216cf78de4d5dea5a760b0f2
root <root@solider245.localdomain>
2020-07-30 15:05:02 +0800

    优化了nav.js文件

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,1 +1,25 @@
-module.exports = [{"text":"文章转载","items":[{"text":"Linux常见命令","link":"/文章转载/Linux常见命令/"},{"text":"Linux常用软件","link":"/文章转载/Linux常用软件/"},{"text":"vim配置","link":"/文章转载/Vim/vim配置/"},{"text":"python","link":"/文章转载/python/"},{"text":"Vim","link":"/文章转载/Vim/"},{"text":"文章转载","link":"/文章转载/"}]},{"text":"常见问题","items":[{"text":"客户端科学上网","link":"/常见问题/客户端科学上网/"},{"text":"命令行科学上网","link":"/常见问题/命令行科学上网/"},{"text":"镜像加速大全","link":"/常见问题/镜像加速大全/"},{"text":"vim","link":"/常见问题/vim/"},{"text":"Git","link":"/常见问题/Git/"}]},{"text":"Pyhton","items":[{"text":"Pyhton","link":"/Pyhton/"}]},{"text":"个人原创","items":[{"text":"个人原创","link":"/个人原创/"}]}];
+module.exports = [
+  {
+    text: "文章转载",
+    items: [
+      { text: "Linux常见命令", link: "/文章转载/Linux常见命令/" },
+      { text: "Linux常用软件", link: "/文章转载/Linux常用软件/" },
+      { text: "vim配置", link: "/文章转载/Vim/vim配置/" },
+      { text: "python", link: "/文章转载/python/" },
+      { text: "Vim", link: "/文章转载/Vim/" },
+      { text: "文章转载", link: "/文章转载/" },
+    ],
+  },
+  {
+    text: "常见问题",
+    items: [
+      { text: "客户端科学上网", link: "/常见问题/客户端科学上网/" },
+      { text: "命令行科学上网", link: "/常见问题/命令行科学上网/" },
+      { text: "镜像加速大全", link: "/常见问题/镜像加速大全/" },
+      { text: "vim", link: "/常见问题/vim/" },
+      { text: "Git", link: "/常见问题/Git/" },
+    ],
+  },
+  { text: "Pyhton", items: [{ text: "Pyhton", link: "/Pyhton/" }] },
+  { text: "个人原创", items: [{ text: "个人原创", link: "/个人原创/" }] },
+];
