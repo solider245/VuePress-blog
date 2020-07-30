0ab9bb4931013ef99c4451e45a48ba6893072b46
root <root@solider245.localdomain>
2020-07-30 15:30:47 +0800

    新建了各种配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,16 @@
-const nav = require("./nav.js"); // 引入刚刚生成的文件
+const nav = require("./config/nav"); // 导航栏配置路径
+const sidebar = require("./config/Sidebar"); //侧边栏配置
+const pluginsConfig = require("./config/PluginsConfig");//插件配置模块
+const markdownConfig= require("./config/markdownConfig");//markdown插件配置模块
+
 module.exports = {
     title: '吴起的个人网站',
     description:'吴起的个人网站，专攻vue,python,linux,PowerBI',
-    plugins: {
-        "vuepress-plugin-auto-sidebar": {
-        nav: true
-        }
-      },
+    markdown:markdownConfig,
+    plugins:pluginsConfig,
     themeConfig:{
-        nav   
+        nav,
+        sidebar,          
     }
 }
 
