e70abf2a671620a9fafd85c8fbb85b51d2c28b2e
root <root@solider245.localdomain>
2020-07-30 19:15:36 +0800

    插件形式由对象式转变成了babel式。 因为babel式可以引用本地插件，而对象式不行

--- docs/.vuepress/config/PluginsConfig.js
+++ docs/.vuepress/config/PluginsConfig.js
@@ -1,12 +1,12 @@
 const moment = require('moment');
-module.exports = {
-  "vuepress-plugin-auto-sidebar": {
+module.exports = [
+  "vuepress-plugin-auto-sidebar", {
     //nav: true,
     sidebarDepth: 3,
   },
-  "@vuepress/back-to-top": {},//插件安装命令: yarn add -D @vuepress/plugin-back-to-top
-  "@vuepress/nprogress": {},//插件安装命令 ： yarn add -D @vuepress/plugin-nprogress
-  "@vuepress/medium-zoom": {
+  "@vuepress/back-to-top", {},//插件安装命令: yarn add -D @vuepress/plugin-back-to-top
+  "@vuepress/nprogress", {},//插件安装命令 ： yarn add -D @vuepress/plugin-nprogress
+  "@vuepress/medium-zoom", {
     selector: "img.zoom-custom-imgs",
     // medium-zoom options here
     // See: https://github.com/francoischalifour/medium-zoom#options
@@ -14,7 +14,7 @@ module.exports = {
       margin: 16,
     },
   },//插件安装命令: yarn add -D @vuepress/plugin-medium-zoom
-  "@vuepress/last-updated": {
+  "@vuepress/last-updated", {
     transformer: (timestamp, lang) => {
       // 不要忘了安装 moment。安装命令： yarn add moment
       const moment = require("moment");
@@ -22,10 +22,10 @@ module.exports = {
       return moment(timestamp).fromNow();
     },
   },
-  '@vuepress/pwa': {
+  '@vuepress/pwa', {
     serviceWorker: true,//如果设置为 true，VuePress 将自动生成并注册一个 Service Worker，用于缓存页面的内容以供离线使用（仅会在生产环境中启用）。
     updatePopup: true  // 本选项开启了一个用于刷新内容的弹窗。这个弹窗将会在站点有内容更新时显示出来，并提供了一个 refresh 按钮，允许用户立即刷新内容。
 },
 //插件使用说明：https://vuepress.vuejs.org/zh/plugin/official/plugin-pwa.html#%E4%BD%BF%E7%94%A8
 //插件安装命令：yarn add -D @vuepress/plugin-pwa
-};
+];
