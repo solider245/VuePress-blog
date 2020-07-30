7c4abaebaa4d2fd4ad38a535670e880b1baa73c1
root <root@solider245.localdomain>
2020-07-30 19:11:27 +0800

    安装了pw插件，并做了说明

--- docs/.vuepress/config/PluginsConfig.js
+++ docs/.vuepress/config/PluginsConfig.js
@@ -22,4 +22,10 @@ module.exports = {
       return moment(timestamp).fromNow();
     },
   },
+  '@vuepress/pwa': {
+    serviceWorker: true,//如果设置为 true，VuePress 将自动生成并注册一个 Service Worker，用于缓存页面的内容以供离线使用（仅会在生产环境中启用）。
+    updatePopup: true  // 本选项开启了一个用于刷新内容的弹窗。这个弹窗将会在站点有内容更新时显示出来，并提供了一个 refresh 按钮，允许用户立即刷新内容。
+},
+//插件使用说明：https://vuepress.vuejs.org/zh/plugin/official/plugin-pwa.html#%E4%BD%BF%E7%94%A8
+//插件安装命令：yarn add -D @vuepress/plugin-pwa
 };
