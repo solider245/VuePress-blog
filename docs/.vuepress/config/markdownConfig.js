4083d7ca2d5294b4dca2ef92a0543723ef2a2dd9
solider <solider245@gmail.com>
2020-08-02 00:12:15 +0800

    修改了目录的级别，从1，2到3，4

--- docs/.vuepress/config/markdownConfig.js
+++ docs/.vuepress/config/markdownConfig.js
@@ -5,7 +5,7 @@ module.exports = {
       //代码块显示行号
       lineNumbers: true ,
       // markdown-it-toc 的选项
-      toc: { includeLevel: [1, 2] }
+      toc: { includeLevel: [1, 2,3,4] }
       //extendMarkdown: md => {
         // 使用更多的 markdown-it 插件!
         //md.use(require('markdown-it-xxx'))
